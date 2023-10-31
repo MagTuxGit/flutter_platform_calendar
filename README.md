bfe0c24ad28bcaac607aaad2b02c6e55485c3499
Andriy Trubchanin <magtuxgit@gmail.com>
2023-10-31 15:33:47 +0200

    readme

--- README.md
+++ README.md
@@ -1,15 +1,40 @@
 # platform_calendar
 
-A new Flutter plugin project.
+A Flutter plugin package to get platform calendar info.
 
-## Getting Started
+## Usage
 
-This project is a starting point for a Flutter
-[plug-in package](https://flutter.dev/developing-packages/),
-a specialized package that includes platform-specific implementation code for
-Android and/or iOS.
+```dart
+class _MyAppState extends State<MyApp> {
+  int _platformFirstDay = 0;
+  final _platformCalendarPlugin = PlatformCalendar();
 
-For help getting started with Flutter development, view the
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
 
+  Future<void> initPlatformState() async {
+    int firstDay = await _platformCalendarPlugin.getFirstDayOfWeek() ?? 0;
+    setState(() {
+      _platformFirstDay = firstDay;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Builder(builder: (context) {
+          final localeFirstDay = MaterialLocalizations.of(context).firstDayOfWeekIndex;
+          return Center(
+            child: Text('Platform first day of week: $platformFirstDay\n\n'
+                'Locale first day of week: $localeFirstDay'),
+          );
+        }),
+      ),
+    );
+  }
+}
+```
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:platform_calendar/platform_calendar.dart';
 
 void main() {
@@ -16,9 +15,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  int _platformFirstDay = 0;
-  final _platformCalendarPlugin = PlatformCalendar();
+  int platformFirstDay = 0;
+  final platformCalendarPlugin = PlatformCalendar();
 
   @override
   void initState() {
@@ -28,25 +26,12 @@ class _MyAppState extends State<MyApp> {
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
-    String platformVersion;
     int firstDay = 0;
     try {
-      platformVersion =
-          await _platformCalendarPlugin.getPlatformVersion() ?? 'Unknown platform version';
-      firstDay =
-          await _platformCalendarPlugin.getFirstDayOfWeek() ?? 0;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
+      firstDay = await platformCalendarPlugin.getFirstDayOfWeek() ?? 0;
+    } catch (_) {}
     setState(() {
-      _platformVersion = platformVersion;
-      _platformFirstDay = firstDay;
+      platformFirstDay = firstDay;
     });
   }
 
@@ -54,25 +39,14 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Builder(
-          builder: (context) {
-            return Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text('Running on: $_platformVersion\n'),
-                  const SizedBox(height: 32),
-                  Text('Platform first day of week: $_platformFirstDay\n'),
-                  const SizedBox(height: 32),
-                  Text('Locale first day of week: ${MaterialLocalizations.of(context).firstDayOfWeekIndex}\n'),
-                ],
-              ),
-            );
-          }
-        ),
+        body: Builder(builder: (context) {
+          final localeFirstDay =
+              MaterialLocalizations.of(context).firstDayOfWeekIndex;
+          return Center(
+            child: Text('Platform first day of week: $platformFirstDay\n\n'
+                'Locale first day of week: $localeFirstDay'),
+          );
+        }),
       ),
     );
   }
